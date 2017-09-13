f5caeb5ba09c42b0c97a451af40a019cbe4b1ae3
Chase Derr <Chisnits@users.noreply.github.com>
2017-09-13 11:40:08 -0600

    How it works

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -5,16 +5,17 @@ class HowItWorks extends Component {
         return (
             <div>
                 <div style={Styles.image}></div>
-                <div>
-                    <p>
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum."
-                    </p>
+                <div style={Styles.titleContainer}>
+                    <p>How It Works</p>
+                </div>
+                <div style={Styles.paragraphWrapper}>
+                    <div style={Styles.paragraphContainer}>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    </div>
                 </div>
             </div>
         );
@@ -26,6 +27,32 @@ var Styles = {
         backgroundImage: `url(${require("../assets/howItWorksFiller.jpg")})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center'
+    },
+    titleContainer: {
+        width: '15vw',
+        maxWidth:'50%',
+        minWidth:'300px',
+        maxHeight:'500px',
+        minHeight:'21%',
+        border: '2px solid black',
+        borderRadius: '25px',
+        boxShadow: '10px 10px 10px #888888',
+        padding: '20px',
+        margin: '15px 0 0 15px',
+        textAlign: 'center'
+    },
+    paragraphWrapper: {
+        display: 'flex',
+        justifyContent: 'space-around'
+    },
+    paragraphContainer: {
+        width: '75%',
+        textAlign: 'left',
+        border: '2px solid black',
+        borderRadius: '25px',
+        boxShadow: '10px 10px 10px #888888',
+        padding: '20px',
+        marginTop: '20px',
     }
 }
 
--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import Radium from 'radium';
 
 class Newsletter extends Component {
   constructor(props) {
@@ -30,16 +31,54 @@ class Newsletter extends Component {
   }
   render() {
     return (
-      <div >
-        <h2 className="footer-headline">Newsletter</h2>
-        <h3 className="newsletter-headline">Subscribe to our newsletter to stay up to date!</h3>
-        <form onSubmit={this.handleSubmit}>
-          <input id="email-input" type="email" placeholder="Email" value={this.state.email} onChange={this.handleEmail} required/>
-          <input id="email-submit" type="submit"/>
+      <div style={Styles.newsletterContainer}>
+        <h2 style={Styles.title} >Newsletter</h2>
+        <h3>Subscribe to our newsletter to stay up to date!</h3>
+        <form style={Styles.formContainer} onSubmit={this.handleSubmit}>
+          <input style={Styles.input} id="email-input" type="email" placeholder="Email" value={this.state.email} onChange={this.handleEmail} required/>
+          <input style={Styles.submit} id="email-submit" type="submit"/>
         </form>
       </div>
     );
   }
 }
+var Styles = {
+  newsletterContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  title: {
+    margin: '0',
+    padding: '0'
+  },
+  formContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  input: {
+    height: '37px',
+    width: '100%',
+    fontSize: '15px',
+    textAlign: 'left',
+    paddingLeft: '1.25em',
+    borderRadius: '8px',
+    backgroundColor: 'white',
+  },
+  submit: {
+    width: '200px',
+    height: '3vh',
+    borderRadius: '10px',
+    border: '1px solid black',
+    marginTop: '10px',
+    backgroundColor: 'white',
+    ':hover': {
+      backgroundColor: '#99ddff',
+      opacity: '.7'
+    }
+  }
+}
+const StyledNewsletter = Radium(Newsletter)
 
-export default Newsletter;
+export default StyledNewsletter;
